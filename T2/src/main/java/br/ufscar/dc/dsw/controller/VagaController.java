dbab23b7ff9d4ad5767a16887cc26ca7cac160a8
Mauricio-HF <108340543+Mauricio-HF@users.noreply.github.com>
2024-09-09 15:54:11 -0300

    vagacontroller.java

--- T2/src/main/java/br/ufscar/dc/dsw/controller/VagaController.java
+++ T2/src/main/java/br/ufscar/dc/dsw/controller/VagaController.java
@@ -5,10 +5,7 @@ import java.util.List;
 
 import jakarta.validation.Valid;
 
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.system.SystemProperties;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -23,7 +20,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.ufscar.dc.dsw.domain.Empresa;
 import br.ufscar.dc.dsw.domain.Usuario;
 import br.ufscar.dc.dsw.domain.Vaga;
-import br.ufscar.dc.dsw.service.impl.EmpresaService;
 import br.ufscar.dc.dsw.service.impl.UsuarioService;
 import br.ufscar.dc.dsw.service.spec.IEmpresaService;
 import br.ufscar.dc.dsw.service.spec.IVagaService;
@@ -32,22 +28,17 @@ import br.ufscar.dc.dsw.service.spec.IVagaService;
 @RequestMapping("/vagas")
 public class VagaController {
 
-	@Autowired
-	private UsuarioService usuarioService;
+    @Autowired
+    private UsuarioService usuarioService;
 
-	@Autowired
-	private IVagaService vagaService;
+    @Autowired
+    private IVagaService vagaService;
 
-	@Autowired
-	private IEmpresaService empresaService;
+    @Autowired
+    private IEmpresaService empresaService;
 
-	@GetMapping("/cadastrar")
-	public String cadastrar(Vaga vaga) {
-		return "vaga/cadastro";
-	}
-
-	@GetMapping("/listar")
-	public String listar(ModelMap model, Principal principal) {
+    @GetMapping("/listar")
+    public String listar(ModelMap model, Principal principal) {
         String username = principal.getName();
         Usuario usuario = usuarioService.buscarPorUsername(username);
 
@@ -59,86 +50,91 @@ public class VagaController {
 
         if (empresa == null) {
             model.addAttribute("vagas", vagaService.buscarTodos());
+        } else {
+            List<Vaga> vagas = vagaService.buscarPorEmpresa(empresa);
+            model.addAttribute("vagas", vagas);
         }
-		else
-		{
-			List<Vaga> vagas = vagaService.buscarPorEmpresa(empresa);
-			model.addAttribute("vagas", vagas);
-		}
 
-		return "vaga/lista";
-	}
+        return "vaga/lista";
+    }
 
-	@PostMapping("/salvar")
-	public String salvar(@Valid Vaga vaga, BindingResult result, RedirectAttributes attr) {
+    @PostMapping("/salvar")
+    public String salvar(@Valid Vaga vaga, BindingResult result, RedirectAttributes attr, Principal principal) {
 
-		if (result.hasErrors()) {
-			return "vaga/cadastro";
-		}
+        if (result.hasErrors()) {
+            return "vaga/cadastro";
+        }
 
-		vagaService.salvar(vaga);
-		attr.addFlashAttribute("sucess", "vaga.create.sucess");
-		return "redirect:/vagas/listar";
-	}
-
-	@GetMapping("/editar/{id}")
-	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
-		model.addAttribute("vaga", vagaService.buscarPorId(id));
-		return "vaga/cadastro";
-	}
-
-	@PostMapping("/editar")
-	public String editar(@Valid Vaga vaga, BindingResult result, RedirectAttributes attr) {
-
-		if (result.hasErrors()) {
-			return "vaga/cadastro";
-		}
-
-		vagaService.salvar(vaga);
-		attr.addFlashAttribute("sucess", "vaga.edit.sucess");
-		return "redirect:/vagas/listar";
-	}
-
-	@GetMapping("/excluir/{id}")
-	public String excluir(@PathVariable("id") Long id, RedirectAttributes attr) {
-		vagaService.excluir(id);
-		attr.addFlashAttribute("sucess", "vaga.delete.sucess");
-		return "redirect:/vagas/listar";
-	}
-
-	@ModelAttribute("empresas")
-	public List<Empresa> listaEmpresas() {
-		return empresaService.buscarTodos();
-	}
-
-/*
-	@GetMapping("listarPorEmpresa")
-    public String listarVagasPorEmpresa(ModelMap model, Principal principal) {
         String username = principal.getName();
         Usuario usuario = usuarioService.buscarPorUsername(username);
-
+        
         if (usuario == null) {
             throw new UsernameNotFoundException("User not found");
         }
 
         Empresa empresa = empresaService.buscarPorUsuario(usuario);
 
+        // Verifica se o usuário está vinculado a uma empresa
         if (empresa == null) {
-            throw new RuntimeException("Empresa not found for the user");
+            throw new RuntimeException("Usuário não vinculado a uma empresa.");
         }
-		List<Vaga> vagas = vagaService.buscarPorEmpresa(empresa);
 
-		System.err.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-		System.err.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-		System.err.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-		System.err.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-		System.err.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-		for(Vaga vaga: vagas)
-		{
-			System.err.println(vaga.getNome());
-		}
-        model.addAttribute("vagas", vagas);
-        return "vaga/lista";
-		
-    }*/
+        vaga.setEmpresa(empresa); // Garante que a vaga esteja associada à empresa correta
+
+        vagaService.salvar(vaga);
+        attr.addFlashAttribute("success", "vaga.create.success");
+        return "redirect:/vagas/listar";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+        model.addAttribute("vaga", vagaService.buscarPorId(id));
+        return "vaga/cadastro";
+    }
+
+    @PostMapping("/editar")
+    public String editar(@Valid Vaga vaga, BindingResult result, RedirectAttributes attr) {
+
+        if (result.hasErrors()) {
+            return "vaga/cadastro";
+        }
+
+        vagaService.salvar(vaga);
+        attr.addFlashAttribute("success", "vaga.edit.success");
+        return "redirect:/vagas/listar";
+    }
+
+    @GetMapping("/excluir/{id}")
+    public String excluir(@PathVariable("id") Long id, RedirectAttributes attr) {
+        vagaService.excluir(id);
+        attr.addFlashAttribute("success", "vaga.delete.success");
+        return "redirect:/vagas/listar";
+    }
+
+    @ModelAttribute("empresas")
+    public List<Empresa> listaEmpresas() {
+        return empresaService.buscarTodos();
+    }
+
+    @GetMapping("/cadastrar")
+    public String cadastrar(Vaga vaga, ModelMap model, Principal principal) {
+        String username = principal.getName();
+        Usuario usuario = usuarioService.buscarPorUsername(username);
+        
+        if (usuario == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+    
+        Empresa empresa = empresaService.buscarPorUsuario(usuario);
+    
+        // Verifica se o usuário está vinculado a uma empresa
+        if (empresa == null) {
+            throw new RuntimeException("Usuário não vinculado a uma empresa.");
+        }
+    
+        vaga.setEmpresa(empresa); // Define a empresa da vaga automaticamente
+        model.addAttribute("vaga", vaga);
+        return "vaga/cadastro";
+    }
 }
+
